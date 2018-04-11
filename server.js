9d3c60f6e730298f24871eecd6550c410f5fff50
singhjaspreet97 <jassi80singh80@gmail.com>
2018-04-11 15:28:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/article-two', function (req, res) {
   res.send('Article two requested and will be served here')
 });
 
-app.get('/article-threee', function (req, res) {
+app.get('/article-three', function (req, res) {
   res.send('Article three requested and will be served here')
 });
 
