9890d7f3269508f101ebbef337c1d00799a37e4d
singhjaspreet97 <jassi80singh80@gmail.com>
2018-04-15 02:06:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,14 +85,16 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
 var counter = 0;
 app.get('/counter', function (req, res) {
     counter = counter + 1;
   res.send(counter.toString());
-});
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
