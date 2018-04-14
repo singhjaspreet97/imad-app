0d4f3adec49477744df4d5fe59ed3228d5e4396c
singhjaspreet97 <jassi80singh80@gmail.com>
2018-04-15 01:40:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
-app.use(morgan('combined'));
+app.use(morgan('combined')); 
 
 var articles= {
      'article-one': {
