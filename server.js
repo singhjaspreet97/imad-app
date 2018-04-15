783561547dca04a267492ad12cf7c4ac0c1fc34f
singhjaspreet97 <jassi80singh80@gmail.com>
2018-04-15 19:00:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,6 +78,13 @@ function createTemplate (data) {
     return htmlTemplate;
 }
 
+
+var counter = 0;
+app.get('/counter', function (req, res) {
+  counter = counter + 1;
+  res.send(`CLicks ${counter.toString()}`);
+});
+
 app.get('/:articleName', function (req, res) {
     //articleName == article-one
     //articles[articleName] == {} content onject for article one
@@ -91,11 +98,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter = 0;
-app.get('/counter', function (req, res) {
-  counter = counter + 1;
-  res.send(`CLicks ${counter.toString()}`);
-});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
