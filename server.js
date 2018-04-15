d1a6b9f4608e9d29c71a5c37ba0b42a27b4507ef
singhjaspreet97 <jassi80singh80@gmail.com>
2018-04-15 18:59:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get('/', function (req, res) {
 var counter = 0;
 app.get('/counter', function (req, res) {
   counter = counter + 1;
-  res.send(counter.toString());
+  res.send(`CLicks ${counter.toString()}`);
 });
 
 app.get('/ui/style.css', function (req, res) {
