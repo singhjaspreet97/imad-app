d3b712473e3288ff3577c8540d7cc772ca6aafcc
singhjaspreet97 <jassi80singh80@gmail.com>
2018-04-24 02:32:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,7 +40,7 @@ submit.onclick = function() {
            //Take some action
             if(request.status === 200)
             {
-                var names = request.responseText
+                var names = request.responseText;
                 names = JSON.parse(names); 
                 var list = '';
                 for(var i=0; i<names.length; i++)
