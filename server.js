43b29580608d3356483aa9b235a968487fcdc406
singhjaspreet97 <jassi80singh80@gmail.com>
2018-04-24 20:17:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/', function (req, res) {
 
 function hash (input, salt) {
    // how do we use hash
-   var hashed = crypto.pbkdff2Sync(input, salt, 10000, 512, 'sha512');
+   var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
    return hashed.toString('hex');
 }
 
