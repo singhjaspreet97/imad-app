b58c81103f7d090860c2bf5c2a1d8ecf7ad9f252
singhjaspreet97 <jassi80singh80@gmail.com>
2018-04-25 16:11:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,31 +1,30 @@
 // Counter code
-var button = document.getElementById('counter');
+ //var button = document.getElementById('counter');
 
-button.onclick = function() {
+ // button.onclick = function() {
     // create a request object
-    var request = new XMLHttpRequest();  
+//    var request = new XMLHttpRequest();  
     
     // capture the response and store it in variable
-    request.onreadystatechange = function()
-    {
-        if(request.readyState === XMLHttpRequest.DONE) {
+  //  request.onreadystatechange = function()
+//    {
+  //      if(request.readyState === XMLHttpRequest.DONE) {
             //Take some action
-            if(request.status === 200) {
-                var counter = request.responseText; 
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
+    //        if(request.status === 200) {
+      //          var counter = request.responseText; 
+    //            var span = document.getElementById('count');
+      //          span.innerHTML = counter.toString();
+    //        }
+     //   }
     // Not done yet
-   };
+  // };
   // MAke a request
-  request.open('GET', 'http://jassi80singh80.imad.hasura-app.io/counter', true);
-  request.send(null);
-};
+ // request.open('GET', 'http://jassi80singh80.imad.hasura-app.io/counter', true);
+ // request.send(null);
+// };
 
-// Submit name 
-
-
+// Submit username,password to login
+ 
 var submit = document.getElementById('submit_btn'); 
 submit.onclick = function() {
     // make a request to the server and send the name
@@ -40,24 +39,29 @@ submit.onclick = function() {
            //Take some action
             if(request.status === 200)
             {
-                var names = request.responseText;
-                names = JSON.parse(names); 
-                var list = '';
-                for(var i=0; i<names.length; i++)
-                    {
-                      list += '<li>' + names[i] + '</li>';
-                    }
-                var ul = document.getElementById('namelist');
-                ul.innerHTML = list;  
+                console.log('user logged in');
+                alert('Logged in successfully');
+            } 
+            else if(request.status === 403)
+            {
+                alert('Username/password is incorrect');
+            }
+            else if(request.status === 500)
+            {
+                alert('Something went wrong on the server');
             }
         }
     // Not done yet
    };
-  // MAke a request
-  var nameInput = document.getElementById('name');
+  // Make a request
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  console.log(username);
+  console.log(password);
   var name = nameInput.value;
-  request.open('GET', 'http://jassi80singh80.imad.hasura-app.io/submit-name?name=' + name, true);
-  request.send(null);
+  request.open('POST', 'http://jassi80singh80.imad.hasura-app.io/login', true);
+  request.setRequestHeader('Content-Type', 'application/json'); 
+  request.send(JSON.stringify({username: username, password: password})); 
     // Capture a list of names and render it as a result
    
 };
